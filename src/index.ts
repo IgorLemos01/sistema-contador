f5c245ac05e8b7b2ab14b467b3493384a8d9c4b4
Igor Lemos Oliveira <127035865+IgorLemos01@users.noreply.github.com>
2026-08-02 01:12:23 -0300

    atualizar

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const mensagem: string = "sistema funcionando";
-console.log(mensagem);
+import express from "express";
+
+const app = express();
+const PORTA = 300;
+
+app.get("/", (req, res) => {
+    res.send("Sistema contador funcionando");
+});
+
+app.listen(PORTA, () => {
+    console.log(`Servidor rodando em http://localhost:${PORTA}`);
+});
+
